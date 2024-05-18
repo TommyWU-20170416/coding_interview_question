6894a015974671a9c6dd4cc58ccc6772e1f2fc92
TommyWU <tommy111811@gmail.com>
2024-05-18 18:18:00 +0800

    [20240518]-更新 MergeString.java

--- leetcode/src/array/easy/MergeString.java
+++ leetcode/src/array/easy/MergeString.java
@@ -1,5 +1,12 @@
 package array.easy;
 
+/**
+ * 1768.https://leetcode.com/problems/merge-strings-alternately/description/
+ *
+ * @author AaronWU
+ * @created 創建時間：2024/05/18 18:15:59
+ * @since JDK8.0
+ */
 public class MergeString {
     public String mergeAlternately(String word1, String word2) {
 //        StringBuilder result = new StringBuilder();
