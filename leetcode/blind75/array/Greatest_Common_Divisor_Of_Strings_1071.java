348dd995960716983903bc3de6f5e4d73f670e80
TommyWU <tommy111811@gmail.com>
2024-05-19 13:17:02 +0800

    [20240519]-新增 String 1071 1768

---
+++ leetcode/blind75/array/Greatest_Common_Divisor_Of_Strings_1071.java
@@ -0,0 +1,101 @@
+package array;
+
+/**
+ * 1071.https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+ *
+ * @author AaronWU
+ * @created 創建時間：2024/05/19 11:39:37
+ * @since JDK8.0
+ */
+public class Greatest_Common_Divisor_Of_Strings_1071 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String gcd1 = s.gcdOfStrings("ABABAB", "ABAB");
+        System.out.println("gcd1: " + gcd1);
+        String gcd2 = s.gcdOfStrings("ABABABAB", "ABAB");
+        System.out.println("gcd2: " + gcd2);
+        String gcd3 = s.gcdOfStrings("leet", "code");
+        System.out.println("gcd3: " + gcd3);
+        String gcd4 = s.gcdOfStrings("ABCDEF", "ABC");
+        System.out.println("gcd4: " + gcd4);
+        String gcd5 = s.gcdOfStrings("ABCABCABC", "ABCAAA");
+        System.out.println("gcd5: " + gcd5);
+    }
+}
+
+class Solution {
+    // ------------------ test1 ------------------
+    /**
+     * <ul>設計思維</ul>
+     * 起初想說先判斷第一位是不是一樣，如果不是就一定不能整除(這樣後面會有問題)
+     * 接著用 str1、str2 長度去找出最大公因數最長的長度
+     * 但這樣做法會失敗 String gcd4 = s.gcdOfStrings("ABCDEF", "ABC");
+     * 這樣會找出 ABC 這答案
+     */
+//    public String gcdOfStrings(String str1, String str2) {
+//        // check the first char
+//        if (str1.charAt(0) != str2.charAt(0)) return "";
+//        int end = gcd(str1.length(), str2.length());
+//        return str2.substring(0, end);
+//    }
+//
+//    public int gcd(int num1, int num2) {
+//        if (num2 == 0) return num1;
+//        return gcd(num2, num1 % num2);
+//    }
+
+    // ------------------ test2 ------------------
+
+    /**
+     * <ul>設計思維</ul>
+     * gcd(a, b) if(a == b) return a
+     * gcd(a, b) if(a > b) gcd(a-b, b)
+     * gcd(a, b) if(a < b) gcd(a, b-a)
+     * a - b 就是從 0 開始把 兩邊有的扣掉
+     * ABABAB, ABAB
+     * => AB, ABAB
+     * => AB, AB => AB
+     */
+//    public String gcdOfStrings(String str1, String str2) {
+//        if (str1.length() > str2.length()) {
+//            int index = 0;
+//            for (int i = 0; i < str2.length(); i++) {
+//                if (str1.charAt(i) == str2.charAt(i)) index++;
+//                else break;
+//            }
+//            if (index == 0) return "";
+//            return gcdOfStrings(str1.substring(index, str1.length()), str2);
+//        } else if (str1.length() < str2.length()) {
+//            int index = 0;
+//            for (int i = 0; i < str1.length(); i++) {
+//                if (str2.charAt(i) == str1.charAt(i)) index++;
+//                else break;
+//            }
+//            if (index == 0) return "";
+//            return gcdOfStrings(str1, str2.substring(index, str2.length()));
+//        } else if (str1.equals(str2)) {
+//            return str1;
+//        }
+//        return "";
+//    }
+
+    // ------------------ test3 ------------------
+
+    /**
+     * <ul>設計思維</ul>
+     * 網路大神補充
+     * 因為是找最大公因數，以字串來說 s = t + t + ... t
+     * 所以 s + t = t + s => (t+t+t...) + t = t + (t+t+t...)
+     * 等於說把 test1 判斷首字，改成更全面的檢查
+     */
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+        int end = gcd(str1.length(), str2.length());
+        return str2.substring(0, end);
+    }
+
+    public int gcd(int num1, int num2) {
+        if (num2 == 0) return num1;
+        return gcd(num2, num1 % num2);
+    }
+}
--- leetcode/blind75/array/MergeString_1768.java
+++ leetcode/blind75/array/MergeString_1768.java
@@ -1,4 +1,4 @@
-package array.easy;
+package array;
 
 /**
  * 1768.https://leetcode.com/problems/merge-strings-alternately/description/
@@ -7,8 +7,14 @@ package array.easy;
  * @created 創建時間：2024/05/18 18:15:59
  * @since JDK8.0
  */
-public class MergeString {
-    public String mergeAlternately(String word1, String word2) {
+public class MergeString_1768 {
+
+    // ------------------ test1 ------------------
+    /**
+     * <ul>設計思維</ul>
+     * 若要方便 Append 字串，可使用 stringbuilder
+     */
+//    public String mergeAlternately(String word1, String word2) {
 //        StringBuilder result = new StringBuilder();
 //        int i = 0;
 //        while (true) {
@@ -22,7 +28,14 @@ public class MergeString {
 //            i++;
 //        }
 //        return result.toString();
+//    }
 
+    // ------------------ test2 ------------------
+    /**
+     * <ul>設計思維</ul>
+     * 因為要調閱字串，當下想到用 char，所以就建立 char[] result，而且這樣省空間
+     */
+    public String mergeAlternately(String word1, String word2) {
         char[] result = new char[word1.length() + word2.length()];
         int index = 0;
         for (int i = 0; i < word1.length() + word2.length(); i++) {
@@ -40,7 +53,7 @@ public class MergeString {
     }
 
     public static void main(String[] args) {
-        MergeString mergeString = new MergeString();
+        MergeString_1768 mergeString = new MergeString_1768();
         String result = mergeString.mergeAlternately("word1", "word2");
         System.out.print(result);
     }
